09b4c8c6b7bdd9e660bfebe6a92bf551b93a9029
Michael Chapman <submariner100@gmail.com>
2017-10-03 19:36:08 +0100

    second commit

--- AR-Portal/ViewController.swift
+++ AR-Portal/ViewController.swift
@@ -23,6 +23,40 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 		self.configuration.planeDetection = .horizontal
 		self.sceneView.session.run(configuration)
 		self.sceneView.delegate = self
+		let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+		self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+		
+	}
+	
+	@objc func handleTap(sender: UITapGestureRecognizer) {
+	
+		guard let sceneView = sender.view as? ARSCNView else {return}
+		let touchLocation = sender.location(in: sceneView)
+		let hitTestResult = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+		if !hitTestResult.isEmpty {
+			self.addPortal(hitTestResult: hitTestResult.first!)
+		} else {
+			print("no surface touched")
+			
+		}
+	}
+	
+	func addPortal(hitTestResult: ARHitTestResult) {
+  			let portalScene = SCNScene(named: "Portal.scnassets/Portal.scn")
+			let portalNode = portalScene!.rootNode.childNode(withName: "Portal", recursively: false)!
+			let transform = hitTestResult.worldTransform
+			let planeXposition = transform.columns.3.x
+		     let planeYposition = transform.columns.3.y
+		     let planeZposition = transform.columns.3.z
+			portalNode.position = SCNVector3(planeXposition, planeYposition, planeZposition)
+		     self.sceneView.scene.rootNode.addChildNode(portalNode)
+		     self.addPlane(nodeName: "roof", portalNode: portalNode, imageName: "top")
+		     self.addPlane(nodeName: "floor", portalNode: portalNode, imageName: "bottom")
+		     self.addWalls(nodeName: "sideDoorA", portalNode: portalNode, imageName: "sideDoorA")
+		     self.addWalls(nodeName: "sideDoorB", portalNode: portalNode, imageName: "sideDoorB")
+		     self.addWalls(nodeName: "sideWallA", portalNode: portalNode, imageName: "sideA")
+		     self.addWalls(nodeName: "sideWallB", portalNode: portalNode, imageName: "sideB")
+		     self.addWalls(nodeName: "backWall", portalNode: portalNode, imageName: "back")
 		
 	}
 
@@ -39,6 +73,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 			DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
 			self.planeDetected.isHidden = true
 		}
+	}
+	
+	func addWalls(nodeName: String, portalNode: SCNNode, imageName: String) {
+		let child = portalNode.childNode(withName: nodeName, recursively: true)
+		child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Portal.scnassets/\(imageName).png")
+		
+	}
+
+	
+	func addPlane(nodeName: String, portalNode: SCNNode, imageName: String) {
+		let child = portalNode.childNode(withName: nodeName, recursively: true)
+		child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Portal.scnassets/\(imageName).png")
+		
+		
+		
+		
 	}
 
 }
